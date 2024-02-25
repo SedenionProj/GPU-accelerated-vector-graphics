782097321c630bc43bd651d8e364addd374525c4
Gerome <quantaleyt@gmail.com>
2024-02-25 20:02:54 +0100

    bézier + trim path

--- GPU-accelerated-vector-graphics/assets/frag.glsl
+++ GPU-accelerated-vector-graphics/assets/frag.glsl
@@ -5,5 +5,5 @@ uniform vec2  u_resolution;
 void main()
 {
     float grad = 1.5*gl_FragCoord.x/u_resolution.x;
-    fragColor = vec4(1-grad,0,grad,1);
+    fragColor = vec4(1-grad,0,grad,1)+0.5;
 }
--- GPU-accelerated-vector-graphics/src/main.cpp
+++ GPU-accelerated-vector-graphics/src/main.cpp
@@ -16,36 +16,84 @@ GLuint CreateSSBO(const std::vector<glm::vec4>& varray)
 
 class LineStrip {
 public:
-	LineStrip(const std::vector<glm::vec4>& vert, float tickness) {
+	LineStrip() = default;
+
+	LineStrip(const std::vector<glm::vec4>& vert, float tickness) : t(1) {
 		vertices = vert;
 		ssbo = CreateSSBO(vert);
 	}
 
 	void draw() {
 		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
-		GLsizei N1 = (GLsizei)vertices.size() - 2;
-		glDrawArrays(GL_TRIANGLES, 0, 6 * (N1 - 1));
+		int nb_lines = size() - 3;
+		int nb_triangles = 6 * int(floor(nb_lines * t) + 1) ;
+		//int nb_triangles_new = nb_triangles % 6 == 0 ? nb_triangles : nb_triangles + 3;
+		glDrawArrays(GL_TRIANGLES, 0, nb_triangles);
 	}
 
-	void update(int pos,const glm::vec4& data) {
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
-		glBufferSubData(GL_SHADER_STORAGE_BUFFER, sizeof(glm::vec4) * (pos), sizeof(glm::vec4), glm::value_ptr(data));
+	void point(int pos, const glm::vec4& data) {
+		vertices[pos] = data;
+		update(pos, data);
 	}
 
 	int size() {
 		return vertices.size();
 	}
 
-private:
+	void trim(float t1) {
+		if (t <= 1) {
+			t = t1;
+
+			float x = (size() - 3) * t;
+
+			int line1 = int(x)+1;
+			int line2 = line1 +1;
+
+			linetest = line1;
+
+			float seg_t = x - floor(x);
+
+			seg2 = seg_t;
+
+			glm::vec4 interpolated = (1.f - seg_t) * vertices[line1] + (seg_t) * vertices[line2];
+
+			update(line2, interpolated);
+		}
+	}
+	float seg2;
+	int linetest;
+
+	float t;
+protected:
+	void update(int pos, const glm::vec4& data) {
+		//vertices[pos] = data;
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
+		glBufferSubData(GL_SHADER_STORAGE_BUFFER, sizeof(glm::vec4) * pos, sizeof(glm::vec4), glm::value_ptr(data));
+	}
 	std::vector<glm::vec4> vertices;
 	int ssbo;
 };
 
+class QuadraticBezier: public LineStrip {
+public:
+	QuadraticBezier(glm::vec4 p0, glm::vec4 p1, glm::vec4 p2) {
+		std::vector<glm::vec4> vert;
+		for (float t = 0; t < 1; t += 0.1) {
+			glm::vec4 pos = (1 - t) * (1 - t) * p0 + (1 - t) * 2 * t * p1 + t * t * p2;
+			vert.push_back(pos);
+		}
+
+		t = 1;
+		vertices = vert;
+		ssbo = CreateSSBO(vert);
+	}
+};
 
 int main()
 {
 	Seden::init(1280, 720, "vector graphics");
-	
+	Seden::initGui();
+
 	Shader sh = Shader("assets/vert.glsl", "assets/frag.glsl");
 	std::vector<glm::vec4> varray1;
 
@@ -53,7 +101,12 @@ int main()
 		varray1.push_back({ i,0,0,1 });
 	}
 	
-	auto lines = LineStrip(varray1,10.);
+	LineStrip lines(varray1,10.);
+
+	std::vector<glm::vec4> varray2 = { glm::vec4(-1,0,0,1),glm::vec4(-0.5,0,0,1),glm::vec4(0.5,0.0,0,1),glm::vec4(0.5,-0.5,0,1),glm::vec4(0.5,-0.6,0,1) };
+	LineStrip segment(varray2,10.);
+
+	QuadraticBezier bezier(glm::vec4( 0,0,0,1 ), glm::vec4( 0.5,0,0,1 ), glm::vec4(1, 1, 0, 1));
 
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
@@ -66,9 +119,19 @@ int main()
 	int w = 1280, h=720;
 	float aspect = (float)w / (float)h;
 	
+	
+
 	while (Seden::isRunning())
 	{
 		Seden::clear();
+		Seden::clearGui();
+
+		ImGui::Begin("test");
+		ImGui::Text(std::to_string(segment.t).c_str());
+		ImGui::Text(std::to_string(segment.linetest).c_str());
+		ImGui::Text(std::to_string(segment.seg2).c_str());
+		ImGui::End();
+
 		
 		glm::vec4 corner = { 1., -glfwGetTime(),0,1 };
 		project = glm::ortho(-aspect, aspect, -1.0f, 1.0f, -5.0f, 10.0f);
@@ -77,18 +140,25 @@ int main()
 
 
 		for (int i = 0; i < lines.size(); i++) {
-			lines.update(i, {(2.*i/lines.size() - 1)*2., sin(glfwGetTime()+i/10.f)/5.f, 0, 1});
+			lines.point(i, {(1.5*i/lines.size() - 1)*1.5, sin(glfwGetTime()+i/10.f)/5.f, 0, 1});
 		}
-
+		lines.trim(glfwGetTime() / 10.);
+		bezier.trim(glfwGetTime() / 10.);
+		segment.trim(glfwGetTime() / 10.);
 
 		sh.setMat4("u_mvp", mvp1);
 		sh.setVec2("u_resolution", glm::vec2(w, h));
-		sh.setFloat("u_thickness", 10.f);
+		sh.setFloat("u_thickness", 20.f);
 		sh.Bind();
-		lines.draw();
 
+		lines.draw();
+		bezier.draw();
+		segment.draw();
+
+		Seden::drawGui();
 		Seden::display();
 	}
 
 	Seden::terminate();
+	Seden::closeGui();
 }
