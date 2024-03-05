69e54fcec0e37b5202a17ff6f1aac4473ddfb5bf
Gerome <quantaleyt@gmail.com>
2024-03-05 20:57:58 +0100

    begin, end path

--- GPU-accelerated-vector-graphics/src/main.cpp
+++ GPU-accelerated-vector-graphics/src/main.cpp
@@ -1,113 +1,17 @@
 ﻿#include <seden.h>
 #include <vector>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include <ft2build.h>
 #include FT_FREETYPE_H
-#include <map>
-GLuint CreateSSBO(const std::vector<glm::vec4>& varray)
-{
-	GLuint ssbo;
-	glGenBuffers(1, &ssbo);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
-	glBufferData(GL_SHADER_STORAGE_BUFFER, varray.size() * sizeof(*varray.data()), varray.data(), GL_STATIC_DRAW);
-	return ssbo;
-}
 
-
-class Path {
-public:
-	Path() = default;
-
-	Path(const std::vector<glm::vec4>& vert, float tickness) : t(0) {
-		vertices = vert;
-		ssbo = CreateSSBO(vert);
-		pathLength = 0;
-		for (int i = 1; i < vertices.size() - 2; i++) {
-			glm::vec2 seg = vertices[i + 1] - vertices[i];
-			pathLength += glm::sqrt(seg.x*seg.x+seg.y*seg.y);
-			std::cout << glm::sqrt(seg.x * seg.x + seg.y * seg.y) << "\n";
-		}
-	}
-
-	void draw() {
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
-		int nb_lines = size() - 3;
-		int nb_triangles = 6 * int(floor(nb_lines * t) + 1);
-		//int nb_triangles_new = nb_triangles % 6 == 0 ? nb_triangles : nb_triangles + 3;
-		glDrawArrays(GL_TRIANGLES, 0, nb_triangles);
-	}
-
-	void update(int pos, const glm::vec4& data) {
-		vertices[pos] = data;
-		updatePath(pos, data);
-	}
-
-	int size() {
-		return vertices.size();
-	}
-
-	void trim(float t1) {
-		if (t <= 1) {
-			int seg_i = 0;
-			float totalLen = 0;
-			float inf = 0;
-			float sup = 0;
-			for (int i = 1; i < size() - 2; i++) {
-				
-				glm::vec2 p1 = vertices[i];
-				glm::vec2 p2 = vertices[i+1];
-				glm::vec2 seg = p2 - p1;
-				
-				
-				float len = glm::sqrt(seg.x * seg.x + seg.y * seg.y)/ pathLength;
-
-				inf = totalLen;
-				sup = totalLen + len;
-
-				seg_i += 1;
-
-				if (t1>=inf && t1<=sup) {
-					break;
-				}
-				
-				totalLen += len;
-			}
-
-			float subdivisions = 1.f / (size() - 3);
-
-			t = ((seg_i-1)+(t1 - inf)/(sup-inf))*subdivisions;
+#include "objects/path.h"
 
 
 
-			float x = (size() - 3) * t;
-
-			int line1 = int(x) + 1;
-			int line2 = line1 + 1;
-
-
-			float seg_t = x - floor(x);
-
-			glm::vec4 interpolated = (1.f - seg_t) * vertices[line1] + (seg_t)*vertices[line2];
-
-			updatePath(line2, interpolated);
-		}
-	}
-	float t;
-	float pathLength;
-protected:
-	void updatePath(int pos, const glm::vec4& data) {
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
-		glBufferSubData(GL_SHADER_STORAGE_BUFFER, sizeof(glm::vec4) * pos, sizeof(glm::vec4), glm::value_ptr(data));
-	}
-	std::vector<glm::vec4> vertices;
-	int ssbo;
-};
 
 int main()
 {
-	Seden::init(1280, 720, "vector graphics");
-	Seden::initGui();
+	Seden::win::init(1280, 720, "vector graphics");
+	Seden::win::initGui();
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_PROGRAM_POINT_SIZE);
@@ -117,7 +21,7 @@ int main()
 	Shader basicSh("assets/shaders/pathVert.glsl", "assets/shaders/pathFrag.glsl");
 	auto projection = glm::ortho(-aspect, aspect, -1.0f, 1.0f, -10.0f, 10.0f);
 
-	std::vector<glm::vec4> varray = { {-1,0,0,1}, {-0.9,0,0,1}, {0.5,0,0,1}, {0.5, 0.1, 0, 1}, {1, 0.1, 0, 1}, {1.1,0.1,0,1} };
+	std::vector<glm::vec4> varray = { {-1,0,0,1}, {-0.9,0,0,1}, {0.5,0,0,1}, {0.5, 0.1, 0, 1}, {1, 0.1, 0, 1}, {1.1, 0.5, 0, 1}, {1.5, 0.7, 0, 1}, {1.7, 0.0, 0, 1} };
 	Path path(varray, 10);
 
 	GLuint vao;
@@ -125,25 +29,30 @@ int main()
 	glBindVertexArray(vao);
 
 	float t = 0.0;
-
-	float last = 0;
-
-	while (Seden::isRunning())
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	float begin=0;
+	float end = 1;
+	while (Seden::win::isRunning())
 	{
-		float newt = glfwGetTime();
-		float dt = newt - last;
-		last = newt;
-
-
-		Seden::clear();
-		Seden::clearGui();
-
+		Seden::win::clear();
+		Seden::win::clearGui();
+		
 		ImGui::Begin("test");
-		ImGui::Text(std::to_string(path.t).c_str());
 		ImGui::Text(std::to_string(glfwGetTime()).c_str());
+		if (ImGui::Button("press")) {
+			
+			
+		}
+		else
+		{
+			
+		}
+		
+		ImGui::SliderFloat("begin", &begin, 0.f, 1.f);
+		ImGui::SliderFloat("end", &end, 0.f, 1.f);
 		ImGui::End();
 
-		t = glfwGetTime();
+		t += .001;
 		
 
 		basicSh.Bind();
@@ -151,19 +60,19 @@ int main()
 		basicSh.setVec2("u_resolution", {1280, 720});
 		basicSh.setFloat("u_thickness", 10);
 
-		path.trim(cos(t)/2.f + 0.5);
-
+		
+		path.trimBegin(begin);
+		path.trimEnd(end);
 		
 
 		path.draw();
-
-
-		Seden::drawGui();
-		Seden::display();
+		
+		Seden::win::drawGui();
+		Seden::win::display();
 	}
 
-	Seden::terminate();
-	Seden::closeGui();
+	Seden::win::terminate();
+	Seden::win::closeGui();
 	//FT_Done_Face(face);
 	//FT_Done_FreeType(library);
 }
---
+++ GPU-accelerated-vector-graphics/src/objects/path.h
@@ -0,0 +1,172 @@
+#pragma once
+
+
+GLuint CreateSSBO(const std::vector<glm::vec4>& varray)
+{
+	GLuint ssbo;
+	glGenBuffers(1, &ssbo);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, varray.size() * sizeof(*varray.data()), varray.data(), GL_STATIC_DRAW);
+	return ssbo;
+}
+
+class Path {
+public:
+	Path() = default;
+
+	Path(const std::vector<glm::vec4>& vert, float tickness){
+		vertices = vert;
+		ssbo = CreateSSBO(vert);
+		pathLength = 0;
+		for (int i = 1; i < vertices.size() - 2; i++) {
+			glm::vec2 seg = vertices[i + 1] - vertices[i];
+			pathLength += glm::sqrt(seg.x * seg.x + seg.y * seg.y);
+		}
+	}
+
+	void draw() {
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
+		int nb_lines = getSegmentNumber();
+		int triangles_end_nb = 6 * int(floor(nb_lines * tEnd) + 1);
+		int triangles_begin_nb = 6 * int(floor(nb_lines * tBegin));
+		
+		glDrawArrays(GL_TRIANGLES, triangles_begin_nb, triangles_end_nb - triangles_begin_nb);
+	}
+
+	void update(int pos, const glm::vec4& data) {
+		vertices[pos] = data;
+		updatePath(pos, data);
+	}
+
+	int size() {
+		return vertices.size();
+	}
+
+	int getSegmentNumber() {
+		return size() - 3;
+	}
+
+	void trimEnd(float end) {
+		end = glm::clamp(end, 0.f, 1.f);
+		tPathEnd = end;
+
+		if (tPathEnd < tPathBegin) {
+			return;
+		}
+
+		
+
+		if (tEnd <= 1) {
+			int segment_i = 0;
+			float trimlLen = 0;
+			float inf = 0;
+			float sup = 0;
+			for (int i = 1; i < size() - 2; i++) {
+
+				glm::vec2 p1 = vertices[i];
+				glm::vec2 p2 = vertices[i + 1];
+				glm::vec2 seg = p2 - p1;
+
+				float len = glm::sqrt(seg.x * seg.x + seg.y * seg.y) / pathLength;
+
+				inf = trimlLen;
+				sup = trimlLen + len;
+
+				segment_i += 1;
+
+				if (end >= inf && end <= sup) {
+					break;
+				}
+
+				trimlLen += len;
+			}
+
+			float subdivisions = 1.f / getSegmentNumber();
+
+			tEnd = ((segment_i - 1) + (end - inf) / (sup - inf)) * subdivisions;
+			
+			
+
+			float whichSeg = getSegmentNumber() * tEnd;
+			int line1 = floor(whichSeg) + 1;
+			int line2 = line1 + 1;
+			float seg_t = whichSeg - floor(whichSeg);
+			glm::vec4 interpolated = (1.f - seg_t) * vertices[line1] + (seg_t)*vertices[line2];
+			updatePath(lastTend, vertices[lastTend]);
+			updatePath(line2, interpolated);
+			lastTend = line2;
+		}
+	}
+
+	void trimBegin(float begin) {
+		begin = glm::clamp(begin, 0.f, 1.f);
+		tPathBegin = begin;
+
+		if (tPathEnd < tPathBegin) {
+			return;
+		}
+
+		
+		if (tBegin <= 1) {
+			int segment_i = 0;
+			float trimlLen = 0;
+			float inf = 0;
+			float sup = 0;
+			for (int i = 1; i < size() - 2; i++) {
+
+				glm::vec2 p1 = vertices[i];
+				glm::vec2 p2 = vertices[i + 1];
+				glm::vec2 seg = p2 - p1;
+
+				float len = glm::sqrt(seg.x * seg.x + seg.y * seg.y) / pathLength;
+
+				inf = trimlLen;
+				sup = trimlLen + len;
+
+				segment_i += 1;
+
+				if (begin >= inf && begin <= sup) {
+					break;
+				}
+
+				trimlLen += len;
+			}
+
+			float subdivisions = 1.f / getSegmentNumber();
+
+			tBegin = ((segment_i - 1) + (begin - inf) / (sup - inf)) * subdivisions;
+
+			
+
+			float whichSeg = getSegmentNumber() * tBegin;
+			int line1 = floor(whichSeg) + 1;
+			int line2 = line1 + 1;
+			float seg_t = whichSeg - floor(whichSeg);
+			glm::vec4 interpolated = (1.f - seg_t) * vertices[line1] + (seg_t)*vertices[line2];
+
+			updatePath(lastTbegin, vertices[lastTbegin]);
+			updatePath(line1, interpolated);
+			lastTbegin = line1;
+		}
+	}
+	
+	float pathLength;
+
+	static Shader shader;
+
+protected:
+	float tPathBegin = 0;
+	float tPathEnd = 1;
+
+	float tBegin = 0;
+	int lastTbegin = 0;
+
+	float tEnd = 1;
+	int lastTend = 1;
+	void updatePath(int pos, const glm::vec4& data) {
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
+		glBufferSubData(GL_SHADER_STORAGE_BUFFER, sizeof(glm::vec4) * pos, sizeof(glm::vec4), glm::value_ptr(data));
+	}
+	std::vector<glm::vec4> vertices;
+	int ssbo;
+};
