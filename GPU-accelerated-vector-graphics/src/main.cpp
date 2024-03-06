5b698784f1ba151d2480ab86bc86c3eae98478ca
Gerome <quantaleyt@gmail.com>
2024-03-06 17:48:28 +0100

    perspective  camera + 3D path (endpoint bug)

--- GPU-accelerated-vector-graphics/src/main.cpp
+++ GPU-accelerated-vector-graphics/src/main.cpp
@@ -6,7 +6,7 @@
 #include "objects/path.h"
 
 
-
+void processInput(Seden::PerspectiveCamera& camera, GLFWwindow* window);
 
 int main()
 {
@@ -20,9 +20,9 @@ int main()
 
 	Shader basicSh("assets/shaders/pathVert.glsl", "assets/shaders/pathFrag.glsl");
 
-	Seden::OrthographicCamera cam(aspect);
+	Seden::PerspectiveCamera cam(aspect);
 
-	std::vector<glm::vec4> varray {{-1,1,0,1}, {-0.5,0,0,1} , {0.5,0,0,1} , {1,1,0,1} };
+	std::vector<glm::vec4> varray {{-1,1,0,1}, {-0.5,0,0,1} , {0.5,0,0,1}  , {0.5,0,1,1}, {0.5,1,1,1}, {1,1,1,1}};
 
 	Path path(varray, 10);
 
@@ -34,7 +34,7 @@ int main()
 
 	float t = 0.0;
 	
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	
 	float begin=0;
 	float end = 1;
@@ -54,18 +54,19 @@ int main()
 			
 		}
 		
+		processInput(cam, Seden::win::getWindowRef());
+
 		ImGui::SliderFloat("begin", &begin, 0.f, 1.f);
 		ImGui::SliderFloat("end", &end, 0.f, 1.f);
 		ImGui::End();
 
 		t += .0001;
 		
-		cam.setZoom(t);
 
 		basicSh.Bind();
-		basicSh.setMat4("u_mvp", cam.getViewProjection());
-		basicSh.setVec2("u_resolution", {1280*t, 720*t});
-		basicSh.setFloat("u_thickness", 50);
+		basicSh.setMat4("u_mvp", cam.getProjectionMatrix()*cam.getViewMatrix());
+		basicSh.setVec2("u_resolution", {1280* glm::length(cam.getPosition()), 720 * glm::length(cam.getPosition())});
+		basicSh.setFloat("u_thickness", 10);
 
 
 		path.trimBegin(begin);
@@ -74,10 +75,10 @@ int main()
 		bezier.trimBegin(begin);
 		bezier.trimEnd(end);
 		
-		bezier.draw();
+		//bezier.draw();
 
 
-		//path.draw();
+		path.draw();
 
 		
 		
@@ -89,4 +90,32 @@ int main()
 	Seden::win::closeGui();
 	//FT_Done_Face(face);
 	//FT_Done_FreeType(library);
+}
+
+void processInput(Seden::PerspectiveCamera& camera, GLFWwindow* window)
+{
+	const float cameraSpeed = 5.f * Seden::win::getDeltaTime();
+	const float sensitivity = 2.f * Seden::win::getDeltaTime();
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.moveFront(cameraSpeed);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.moveFront(-cameraSpeed);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.moveRight(-cameraSpeed);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.moveRight(cameraSpeed);
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		camera.moveUp(cameraSpeed);
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+		camera.moveUp(-cameraSpeed);
+
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		camera.rotate(0.0, -sensitivity);
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		camera.rotate(0.0, sensitivity);
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		camera.rotate(sensitivity, 0.0);
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		camera.rotate(-sensitivity, 0.0);
 }
